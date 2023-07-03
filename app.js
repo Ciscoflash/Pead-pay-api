6621143f128edf2ea5a954233045d7506a29d351
Davidon4 <Quayson450@gmail.com>
2023-07-03 08:45:39 +0100

    'update'

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const port = 5000;
-const Treblle = require("@treblle/express");
-const cors = require("cors");
+const express = require('express')
+const app = express()
+const port = 5000
 
-// Initialize Cores with allowed origins
-app.use(cors(require("./Config/corsOptions")));
-
-// Initializing the Treblle Sdk as a global instance
-app.use(
-  Treblle({
-    apiKey: process.env.TREBLLE_API_KEY,
-    projectId: process.env.TREBLLE_PROJECT_ID,
-  })
-);
-
-app.get("/", (req, res) => {
-  res.send("Welcome to PEAD-PAY Payment Server");
-});
-
-app.use("/api/v1/payment", require("./Routes/Payment"));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
-});
+  console.log(`App is listening on port ${port}`)
+})
