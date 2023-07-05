a3325e8176f0ffbfc4fa358e51f3cca61fd1bbe9
Ciscoflash <abrahamjude1999@gmail.com>
2023-07-05 22:18:48 +0100

    effecting my changes

--- Config/mongoDBConfig.js
+++ Config/mongoDBConfig.js
@@ -1,29 +1,48 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// require('dotenv').config();
 
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(process.env.MONGODB_URI, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGODB_URI, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+// console.log(process.env.MONGODB_Uri)
 
-console.log(process.env.MONGODB_Uri)
+// async function run() {
+//     try {
+//         // Connect the client to the server	(optional starting in v4.7)
+//         await client.connect();
+//         // Send a ping to confirm a successful connection
+//         await client.db("admin").command({ ping: 1 });
+//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
 
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
+// run().catch(console.dir);
+const mongoose = require("mongoose");
 
-run().catch(console.dir);
+const connectDb = () => {
+  const config = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  };
+
+  // const url = "mongodb+srv://WEB3IQ:33bYAOgWrpVFZgNW@cluster1.ebin1dx.mongodb.net/Web3IQDb"
+  // const uri = 'mongodb://127.0.0.1:27017/testCase'
+  mongoose.connect(process.env.MONGODB_URI, config);
+  mongoose.connection.on("open", () => {
+    console.log("Server Connected");
+  });
+  mongoose.connection.on("error", (e) => {
+    console.log(e);
+  });
+};
+
+module.exports = connectDb;
--- Routes/Payment.js
+++ Routes/Payment.js
@@ -1,9 +1,70 @@
+require("dotenv").config();
 const express = require("express");
 const app = express.Router();
 const treblle = require("@treblle/express");
+const https = require("https");
+const paymentModel = require("../Model/Payment");
 
-app.route("/api/v1/").post(async (req, res) => {
-  res.json("Welcome the official post request for payment");
+app.route("/").post(async (req, res) => {
+  let data;
+  const params = JSON.stringify({
+    email: req.body.email,
+    amount: req.body.amount,
+  });
+
+  const options = {
+    hostname: "api.paystack.co",
+    port: 443,
+    path: "/transaction/initialize",
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.PAYSTACK_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const reqpaystack = https
+    .request(options, (respaystack) => {
+      data = "";
+
+      respaystack.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      respaystack.on("end", () => {
+        // res.send(data);
+        res.json({
+          code: 200,
+          msg: "payment saved successfully proceed with the link",
+          data: new_payment,
+          paystack: data,
+        });
+      });
+      // console.log(JSON.parse(data));
+    })
+    .on("error", (error) => {
+      console.error(error);
+    });
+
+  // if the paystack status is true then save the user information
+  const payment = new paymentModel({
+    _id: req.body._id,
+    customer_id: req.body.customer_id,
+    email: req.body.email,
+    amount: req.body.amount,
+    currency: req.body.currency,
+    bank: req.body.bank,
+    description: req.body.description,
+    payment_status: req.body.payment_status,
+  });
+
+  if (!payment)
+    return response.json({ code: 402, message: "payment required" });
+
+  const new_payment = await payment.save();
+
+  reqpaystack.write(params);
+  reqpaystack.end();
 });
 
 module.exports = app;
