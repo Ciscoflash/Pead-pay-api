62931f6e163aad220f9154f48be3fe61bc35b60f
Davidon4 <Quayson450@gmail.com>
2023-07-05 08:08:30 +0100

    'update'

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const treblle = require('@treblle/express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
 require("dotenv").config();
 const run = require('./Config/mongoDBConfig')
 const port = 5000
@@ -9,13 +12,13 @@ app.use(cors(require("./Config/corsOptions")));
 
 // Initializing the Treblle Sdk as a global instance
 app.use(
-  Treblle({
+  treblle({
     apiKey: process.env.TREBLLE_API_KEY,
     projectId: process.env.TREBLLE_PROJECT_ID,
   })
 );
 
-app.use(express.urlencoded(true));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
