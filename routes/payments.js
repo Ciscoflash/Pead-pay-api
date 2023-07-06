f7ae1015aaafc536c9b15bec19647e9bcc865a6c
Ciscoflash <abrahamjude1999@gmail.com>
2023-07-06 10:20:33 +0100

    current changes

--- routes/payments.js
+++ routes/payments.js
@@ -1,6 +1,8 @@
-
 const router = require("express").Router();
 const paymentController = require("../controllers/PaymentController");
+const compression = require("compression");
 
 router.post("/", paymentController.payment);
+router.get("/", paymentController.getPayment);
+router.get("/paystack", paymentController.paystackList);
 module.exports = router;
