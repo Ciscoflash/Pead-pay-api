c76ce32a6eddd2d0e02a6d813b9bfa6a32b5efd6
Ciscoflash <abrahamjude1999@gmail.com>
2023-07-06 12:21:17 +0100

    updating my branch

--- routes/payments.js
+++ routes/payments.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const verifyJwt = require("../middlewares/verifyJwt")
+const verifyJwt = require("../middlewares/verifyJwt");
 const paymentController = require("../controllers/PaymentController");
 
-router.post("/",verifyJwt(), paymentController.payment);
-router.get("/",verifyJwt(), paymentController.getPayment);
+router.post("/", verifyJwt(), paymentController.payment);
+router.get("/", verifyJwt(), paymentController.getPayment);
 router.get("/paystack", paymentController.paystackList);
 module.exports = router;
